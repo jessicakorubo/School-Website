e6ae86a6261b6ead5d91fd8c8ac7f8f38019c024
Jessica <jessicakorubo@gmail.com>
2023-08-09 09:52:18 +0100

    Finished website

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const body = document.querySelector('body');
+var reviews = document.querySelectorAll('.reviews');
+
+var testimonialSlide = document.querySelector('.quotes');
+
 var image = document.getElementById("slide-image");
 document.querySelector("img").style.cssText=
 `opacity: .7;
@@ -18,28 +23,79 @@ function slide()
 }
 setInterval("slide()",3000);
 
-var header = document.querySelector("#header");
-document.addEventListener('scroll', ()=>{
-    if (window.scrollY > 150){
-        header.style.backgroundColor="#fff";
+// var header = document.querySelector("#header");
+
+
+
+
+
+// const scrollbarWidth = document.body.offsetWidth - document.body.clientWidth;
+// console.log(scrollbarWidth);
+ 
+// For the testimonial slide 
+var  left = document.querySelector('.left')
+var  right = document.querySelector('.right')
+
+
+reviews.forEach((rev) => {
+    console.log(rev, 'the review');
+    reviewWidth = rev.clientWidth;
+})
+
+ let x = 0;
+   let widths = reviewWidth* 3;
+   var slider = -(widths+(40*3));
+console.log(slider, 'widths 40');
+right.onclick = function() {
+    console.log('You only clicked', x);
+    console.log(slider, 'slider right now');
+ 
+  
+    if (x <= (widths)) {
+        left.classList.remove('inactive');
+        console.log('right is working');
+        x = x - reviewWidth-40;
+        testimonialSlide.style.left = x + "px";
+        console.log(x, "and the x is");
+        if (x == slider){
+        right.classList.add('inactive');
+        }
+        else {
+            right.classList.remove('inactive');
+        }
+    }
+    if  (x == 0 ){
+    left.classList.remove("inactive");
+    }
+   
+}
+ console.log(widths, "wdithje");
+ if(x==0) {
+    left.classList.add('inactive');
+}
+left.onclick = function() {
+    console.log('You only clicked', x)
+    if (x <= reviewWidth){
+        left.classList.remove('inactive');
+        console.log('left is working')
+        x = x + reviewWidth+40;
+        console.log(x, 'after clicking on left button');
+        testimonialSlide.style.left = x + "px";
+        if (x>slider) {
+            right.classList.remove('inactive');
+        }
     }
     else {
-        header.style.backgroundColor="transparent";
+        right.classList.remove('inactive');
     }
-})
+    if(x==0) {
+    left.classList.add('inactive');
+}
+}
 
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-hamburger');
 
-
-// MAKING THE TESTIMONIALS ACTIVE
-var review =document.querySelector('.reviews');
-testimonial = document.querySelector('.TesT p');
-anabelle = document.querySelector('.anabelle');
-milton= document.querySelector('.milton');
-review.addEventListener('click', ()=>{
-    if (review.classList.has('anabelle')){
-        testimonial.innerHtml = anabelle;
-    }
-    else if (review.classList.has('milton')) {
-        testimonial.innerHtml = milton;
-    }
-})
+hamburger.onclick = function() {
+    menu.classList.toggle('active');   
+}
